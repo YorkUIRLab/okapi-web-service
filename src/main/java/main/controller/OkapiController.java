657d5b4b728e79ab6f3a217f43467daad268d7a8
sadrayan <sadrayan@gmail.com>
2016-02-15 12:04:44 -0800

    okapi integration

--- src/main/java/main/controller/OkapiController.java
+++ src/main/java/main/controller/OkapiController.java
@@ -1,13 +1,16 @@
 package main.controller;
 
-import main.service.OkapiService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import main.service.OkapiService;
+
 import java.util.Collection;
+import java.util.List;
 
 /**
  * PackALunch
@@ -17,20 +20,58 @@ import java.util.Collection;
 
 public class OkapiController {
 
-    @Autowired
+ 
     private OkapiService okapiService;
-
-    @RequestMapping(value = "/database", method= RequestMethod.GET)
-    public String getDataBase() {
-        return "not implemented yet.";
+    
+    public OkapiController () {
+    	okapiService = new OkapiService();
     }
 
-    @RequestMapping(value = "/database",
-            method=RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+
+
+    @RequestMapping(value = "/database", method= RequestMethod.GET)
+    public ResponseEntity<String> getDataBase() {
+        String response = okapiService.listDatabase();
+        return new ResponseEntity<> (response, HttpStatus.OK);
+
+    }
+
+    @RequestMapping(value = "/database", method=RequestMethod.POST)
     public ResponseEntity<String> setDatabase(@RequestParam(value="dbname") String name) {
-        System.out.println("in create" + name);
+        System.out.println("in create " + name);
         String response = okapiService.setDatabase(name);
         return new ResponseEntity<> (response, HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/parseterms", method=RequestMethod.POST)
+    public ResponseEntity<String> parseTerms(@RequestParam(value="parseTerms") String parseTerms) {
+        String response = okapiService.parseTerms(parseTerms);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/stemterm",  method=RequestMethod.POST)
+    public ResponseEntity<String> stemTerm(@RequestParam(value="stemTerm") String stemTerm) {
+        String response = okapiService.stemTerm(stemTerm);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/searchterm", method=RequestMethod.POST)
+    public ResponseEntity<String> searchTerm(@RequestParam(value="searchTerm") String searchTerm) {
+        String response = okapiService.searchTerm(searchTerm);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getweigh", method=RequestMethod.POST)
+    public ResponseEntity<String> getWeigh(@RequestParam(value="term") String term,
+                                           @RequestParam(value="searchResult") String searchResult) {
+        String response = okapiService.getWeigh(term, searchResult);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/result", method= RequestMethod.GET)
+    public ResponseEntity<String> displayResultSet() {
+        String response = okapiService.displayResultSet();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    
 }
--- src/main/java/main/service/OkapiService.java
+++ src/main/java/main/service/OkapiService.java
@@ -1,38 +1,94 @@
 package main.service;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+import org.javokapi;
 import org.springframework.stereotype.Service;
 
+import main.factory.OkapiFactory;
+
+
+
 /**
- * PackALunch
+ * Sadra
  * Created by Sadra on 1/18/16.
  */
 @Service
 public class OkapiService {
+	
+	javokapi okapiInterface = null;
+	Utils okapiUtils = null;
+	
 
-    //TODO: load okapi interface from Okapi package;
+		
+	public OkapiService ()  {
+		OkapiFactory okapiFactory = new OkapiFactory ();
+		okapiInterface = okapiFactory.getInstance();
+		okapiUtils = new Utils();		
+	}
+	
+
+    public String listDatabase() {
+    	System.out.println("in list Database...");
+        String dbList = okapiInterface.infoDB();
+        System.out.println("Listing Available Databases:" +dbList); //OUTPUTS THE AVAILABLE DATABASES
+        return dbList;
+    }
+
 
     public String setDatabase(String dbName) {
-        return null;
+        System.out.println("DB name: " + dbName);
+        System.out.println(okapiInterface.displayStemFunctions());
+        okapiInterface.chooseDB(dbName); //CHOOSE THE DATABASE UESR INPUTTED
+        okapiInterface.deleteAllSets(); //CLEAR ALL EXISTING DATASETS IF ANY
+        return "database name successfully set to " + dbName;
     }
+
 
     public String parseTerms(String parseTerms) {
-        return null;
+        String okapiParsedTerms = okapiInterface.superParse("", parseTerms);
+        System.out.println("Parsed term(s): " + okapiParsedTerms);  //OUTPUT THE PARSED TERMS
+        return "parsed terms: " + okapiParsedTerms;
     }
+
 
     public String stemTerm(String term) {
-        return null;
+        String stemmedWord = okapiInterface.stem("psstem", term); //FINDS THE ROOT OF THE WORD (i.e. 'running' becomes 'run')
+        System.out.println("Stemmed word: " + stemmedWord);
+        return stemmedWord.replace("t=", "");  //Trims the t= produced from the stem() function
     }
 
+ 
     public String searchTerm(String term) {
-        return null;
+        String searchResult = okapiInterface.find("1", "0", "DEFAULT", term); //QUERIES OKAPI WITH THE SEARCH WORD ENTERED BY USER
+        System.out.println("search result: " + searchResult);
+        return searchResult;
     }
 
-    public String getWeigh(String term, String searchResult) {
-        return null;
-    }
 
     public String displayResultSet() {
-        return null;
+        System.out.println("Your query returned the following results");
+        String results = "";
+
+        for (int i = 0; i < 99; i++) {
+            results = okapiInterface.showSetRecord("1", "1", Integer.toString(i), "0") + ", ";
+        }
+
+        System.out.println("Record #" + results);
+        return results;
     }
+
+
+    public String getWeigh(String term, String searchResult) {
+        String np = okapiUtils.findNP(searchResult, term);  //SEE METHOD findNP() BELOW
+        int npInt = Integer.parseInt(np);
+
+        String weight = okapiInterface.weight("2", np, "0", "0", "4", "5");
+        System.out.println("weight()= " + weight);  //OUTPUT THE WEIGHT FOUND BY THE weight() FUNCTION
+        System.out.println("setFind()= " + okapiInterface.setFind("0", weight, "")); //OUTPUT THE SET
+        System.out.println(okapiInterface.displayStemFunctions());
+        return weight;
+    }
+
 }
